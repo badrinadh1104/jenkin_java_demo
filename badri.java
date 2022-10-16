38d15706c84607c0fbd32ec7f53b95bb7b6c940f
Badrinadh Ande <iambadrinath9@gmail.com>
2022-10-16 11:47:32 +0530

    commit-2 by badrinadh

--- badri.java
+++ badri.java
@@ -1,5 +1,6 @@
 public class badri{
 	public static void main(String[] args){
 		System.out.println("this is note pad written java code by badrinadh");
+		System.out.println("this is git comit and jenkin compliation");
 	}
 }
